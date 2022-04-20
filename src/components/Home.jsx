325ff87be1e554fccf9ac78d6fcaf924141fa1e9
Karan16-prog <karan.s1606@gmail.com>
2022-04-20 22:29:20 +0530

    links changed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,9 +15,8 @@ const Home = () => {
           I'm a React and React Native Developer
         </h2>
         <p className="text-[#8892b0] py-4 max-w-[700px]">
-          I'm a MERN stack developer specializing in building exceptional
-          digital experiences. Currently, I'm focused on building responsive web
-          applications
+          Adaptable professional with a few months of experience and proven knowledge of web development using MERN stack.
+          Aiming to leverage my skills to successfully fill the Front End Developer role at your company.
         </p>
         <div>
           <Link to="work" smooth={true} duration={500}>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -62,6 +62,15 @@ const Navbar = () => {
             Projects
           </Link>
         </li>
+        <li>
+            <a
+              target="_blank"
+              className="flex justify-between items-center w-full text-gray-300"
+              href="https://drive.google.com/file/d/1uAOLNG_t7aQA9aiYf_Bke_z-rFceD3lu/view?usp=sharing"
+            >
+            Resume
+            </a>
+        </li>
       </ul>
 
       {/* Hamburger */}
@@ -77,7 +86,7 @@ const Navbar = () => {
             : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
         }
       >
-        <li className="py-6 text-4xl">
+        <li className="py-6 text-3xl">
           <Link
             onClick={handleClick}
             to="home"
@@ -87,7 +96,7 @@ const Navbar = () => {
             Home
           </Link>
         </li>
-        <li className="py-6 text-4xl">
+        <li className="py-6 text-3xl">
           <Link
             onClick={handleClick}    
             to="about"
@@ -97,7 +106,7 @@ const Navbar = () => {
             About
           </Link>
         </li>
-        <li className="py-6 text-4xl">
+        <li className="py-6 text-3xl">
           <Link
             onClick={handleClick}
             to="skills"
@@ -107,8 +116,8 @@ const Navbar = () => {
             Skills
           </Link>
         </li>
-        <li className="py-6 text-4xl">
-          {" "}
+        <li className="py-6 text-3xl">
+          {/* {" "} */}
           <Link
              onClick={handleClick}
             to="work"
@@ -118,6 +127,50 @@ const Navbar = () => {
             Projects
           </Link>
         </li>
+        <li className="py-6 text-3xl">
+          {/* {" "} */}
+          <a
+              target="_blank"
+              onClick={handleClick}
+              className="flex justify-between items-center w-full text-gray-300"
+              href="https://drive.google.com/file/d/1uAOLNG_t7aQA9aiYf_Bke_z-rFceD3lu/view?usp=sharing"
+            >
+            Resume
+            </a>
+        </li>
+         <li className="py-6 text-3xl">
+          {/* {" "} */}
+          <a
+              target="_blank"
+              onClick={handleClick}
+              className="flex justify-between items-center w-full text-gray-300"
+              href="https://github.com/Karan16-prog"
+            >
+            Github
+            </a>
+        </li>
+        <li className="py-6 text-3xl">
+          {/* {" "} */}
+          <a
+              target="_blank"
+              onClick={handleClick}
+              className="flex justify-between items-center w-full text-gray-300"
+              href="mailto:karan.s1606@gmail.com"
+            >
+            Email
+            </a>
+        </li>
+        <li className="py-6 text-3xl">
+          {/* {" "} */}
+          <a  
+              target="_blank"
+              onClick={handleClick}
+              className="flex justify-between items-center w-full text-gray-300"
+              href="https://www.linkedin.com/in/karan-singh-08a1a3126/"
+            >
+            LinkedIn
+            </a>
+        </li>
       </ul>
 
       {/* Social icons */}
@@ -125,6 +178,7 @@ const Navbar = () => {
         <ul>
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]">
             <a
+              target="_blank"
               className="flex justify-between items-center w-full text-gray-300"
               href="https://github.com/Karan16-prog"
             >
@@ -133,6 +187,7 @@ const Navbar = () => {
           </li>
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]">
             <a
+              target="_blank"
               className="flex justify-between items-center w-full text-gray-300"
               href="mailto:karan.s1606@gmail.com"
             >
@@ -141,14 +196,16 @@ const Navbar = () => {
           </li>
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
             <a
+              target="_blank"
               className="flex justify-between items-center w-full text-gray-300"
-              href="/"
+              href="https://drive.google.com/file/d/1uAOLNG_t7aQA9aiYf_Bke_z-rFceD3lu/view?usp=sharing"
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
           </li>
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
             <a
+              target="_blank"
               className="flex justify-between items-center w-full text-gray-300"
               href="https://www.linkedin.com/in/karan-singh-08a1a3126/"
             >
